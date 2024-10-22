402a05d4b05d429f346ac830bc9caaef83518211
Егор Комов <egorkomoval@yandex.ru>
2024-10-23 00:44:35 +0300

    Add files via upload

---
+++ Lab7_1/Source.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <locale.h>
+
+int main()
+{
+    setlocale(LC_ALL, "RUS");
+    char c;
+    printf("Введите значение: ");
+    scanf_s("%c", &c);
+    switch (c)
+    {
+    case '0':
+    case '1':
+    case '2':
+    case '3':
+    case '4':
+    case '5':
+    case '6':
+    case '7':
+    case '8':
+    case '9': {
+        printf("Цифра\n");
+        break;
+    }
+    case 'a':
+    case 'b':
+    case 'c':
+    case 'd':
+    case 'e':
+    case 'f':
+    case 'g':
+    case 'h':
+    case 'i':
+    case 'j':
+    case 'k':
+    case 'l':
+    case 'm':
+    case 'n':
+    case 'o':
+    case 'p':
+    case 'q':
+    case 'r':
+    case 's':
+    case 't':
+    case 'u':
+    case 'v':
+    case 'w':
+    case 'x':
+    case 'y':
+    case 'z':
+        printf("Буква\n");
+        break;
+    default:
+        printf("Неизвестный символ\n");
+    }
+
+    return 0;
+}
